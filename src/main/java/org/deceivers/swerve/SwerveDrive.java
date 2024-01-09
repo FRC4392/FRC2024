005ad0fbb7a119bdad2c7b67f8919bf8ccd226a7
Kristopher Zakfeld <kzakfeld@gmail.com>
2024-01-08 21:18:02 -0500

    initial import

---
+++ src/main/java/org/deceivers/swerve/SwerveDrive.java
@@ -0,0 +1,187 @@
+package org.deceivers.swerve;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.DoubleArrayPublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import java.util.Arrays;
+import java.util.function.DoubleSupplier;
+
+// import com.pathplanner.lib.PathPlannerTrajectory;
+// import com.pathplanner.lib.PathPlannerTrajectory.EventMarker;
+// import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+
+public class SwerveDrive {
+    //Swerve Devices
+    private final SwerveModule[] mModules;
+    private final int numModules;
+    private final SwerveDriveKinematics mKinematics;
+    private final SwerveDrivePoseEstimator mSwerveDrivePoseEstimator;
+    private final HolonomicDriveController mDriveController;
+    private final DoubleSupplier mGyroAngle;
+    private final ProfiledPIDController rotationPIDController = new ProfiledPIDController(15,.1,.1,new TrapezoidProfile.Constraints(500, 500));
+
+    //Network Table Data
+    private final NetworkTableInstance networkTableInstance = NetworkTableInstance.getDefault();
+    private final DoubleArrayPublisher pathGoalPose;
+    private final DoubleArrayPublisher pathGoalPoseError;
+    private final DoubleArrayPublisher swervePose;
+
+    public SwerveDrive(DoubleSupplier gyroAngle, SwerveModule... modules){
+
+        mGyroAngle = gyroAngle;
+        numModules = modules.length;
+        mModules = Arrays.copyOf(modules, numModules);
+
+        //Network Table setup
+        NetworkTable swerveTable = networkTableInstance.getTable("swervetable");
+        pathGoalPose = swerveTable.getDoubleArrayTopic("/swervetable/pathgoalpose").publish();
+        pathGoalPoseError = swerveTable.getDoubleArrayTopic("/swervetable/pathgoalposeerror").publish();
+        swervePose = swerveTable.getDoubleArrayTopic("/swervetable/swervepose").publish();
+
+        Translation2d[] moduleLocations = new Translation2d[numModules];
+        for (int i = 0; i < numModules; i++){
+            moduleLocations[i] = mModules[i].getModuleLocation();
+        }
+
+        mKinematics = new SwerveDriveKinematics(moduleLocations);
+
+        rotationPIDController.enableContinuousInput(-Math.PI, Math.PI);
+
+        mDriveController = new HolonomicDriveController(new PIDController(4,0,0), new PIDController(4,0,0), rotationPIDController);
+
+        SwerveModulePosition[] states = new SwerveModulePosition[numModules];
+        for (int i = 0; i < numModules; i++) {
+            states[i] = mModules[i].getPosition();
+        }
+
+        mSwerveDrivePoseEstimator = new SwerveDrivePoseEstimator(mKinematics, Rotation2d.fromDegrees(mGyroAngle.getAsDouble()), states, new Pose2d(), VecBuilder.fill(0.05, 0.05, Units.degreesToRadians(5)),VecBuilder.fill(0.5, 0.5, Units.degreesToRadians(360)));
+
+        Arrays.stream(mModules).forEach(SwerveModule::init);
+    }
+
+    public void setLocation(double x, double y, double angle){
+        Pose2d newPose = new Pose2d(x, y, Rotation2d.fromDegrees(angle));
+        SwerveModulePosition[] states = new SwerveModulePosition[numModules];
+        for (int i = 0; i < numModules; i++) {
+            states[i] = mModules[i].getPosition();
+        }
+
+        mSwerveDrivePoseEstimator.resetPosition(Rotation2d.fromDegrees(mGyroAngle.getAsDouble()), states, newPose);
+        rotationPIDController.reset(angle);
+    }
+
+    public void setModulesAngle(double angle, int module){
+        mModules[module].setAngle(angle);
+    }
+
+    public Pose2d getPose(){
+        return mSwerveDrivePoseEstimator.getEstimatedPosition();
+    }
+
+    public void updatePoseWithVision(Pose2d newPose, double time){
+        mSwerveDrivePoseEstimator.addVisionMeasurement(newPose, time);
+    }
+
+    public ChassisSpeeds getChassisSpeeds(){
+        SwerveModuleState[] states = new SwerveModuleState[numModules];
+        for (int i = 0; i < numModules; i++) {
+            states[i] = mModules[i].getState();
+        }
+        ChassisSpeeds chassisSpeeds = mKinematics.toChassisSpeeds(states);
+        return(chassisSpeeds);
+    }
+
+	public void stop() {
+        Arrays.stream(mModules).forEach(SwerveModule::stop);
+	}
+
+    public void drive(double forward, double strafe, double azimuth, boolean fieldRelative){
+        ChassisSpeeds speeds;
+        if (fieldRelative){
+            speeds = ChassisSpeeds.fromFieldRelativeSpeeds(forward, strafe, azimuth, Rotation2d.fromDegrees(mGyroAngle.getAsDouble()-180));
+        } else {
+            speeds = new ChassisSpeeds(forward, strafe, azimuth);
+        }
+        SwerveModuleState[] states = mKinematics.toSwerveModuleStates(speeds);
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, 1);
+        for (int i = 0; i < numModules; i++){
+            mModules[i].set(states[i]);
+        }
+    }
+
+    public void driveClosedLoop(double forward, double strafe, double azimuth, boolean fieldRelative){
+        ChassisSpeeds speeds;
+        if (fieldRelative){
+            speeds = ChassisSpeeds.fromFieldRelativeSpeeds(forward, strafe, azimuth, Rotation2d.fromDegrees(mGyroAngle.getAsDouble()));
+        } else {
+            speeds = new ChassisSpeeds(forward, strafe, azimuth);
+        }
+        SwerveModuleState[] states = mKinematics.toSwerveModuleStates(speeds);
+        //SwerveDriveKinematics.normalizeWheelSpeeds(states, 1);
+        for (int i = 0; i < numModules; i++){
+            mModules[i].setClosedLoop(states[i]);
+        }
+    }
+
+    public Pose2d updateOdometry(){
+        SwerveModulePosition[] states = new SwerveModulePosition[numModules];
+        for (int i = 0; i < numModules; i++) {
+            states[i] = mModules[i].getPosition();
+        }
+
+        return mSwerveDrivePoseEstimator.update(Rotation2d.fromDegrees(mGyroAngle.getAsDouble()), states);
+    }
+
+    // public void followPath(double startTime, PathPlannerTrajectory pptrajectory, boolean useLimelight){
+    //     PathPlannerState goal = (PathPlannerState) pptrajectory.sample(Timer.getFPGATimestamp() - startTime);
+        
+
+    //     ChassisSpeeds speeds = mDriveController.calculate(getPose(), goal, Rotation2d.fromDegrees(goal.holonomicRotation.getDegrees()));
+
+    //     driveClosedLoop(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond, speeds.omegaRadiansPerSecond, false);
+
+    //     double[] pathGoalArray = {goal.poseMeters.getX(), goal.poseMeters.getY(), goal.holonomicRotation.getDegrees()};
+    //     pathGoalPose.set(pathGoalArray);
+    //     SmartDashboard.putNumber("GoalX", goal.poseMeters.getX());
+    //     SmartDashboard.putNumber("GoalY", goal.poseMeters.getY());
+    //     SmartDashboard.putNumber("GoalRotation", goal.poseMeters.getRotation().getDegrees());
+    //     double[] pathErrorArray = {goal.poseMeters.getX()-getPose().getX(), goal.poseMeters.getY()-getPose().getY(), goal.holonomicRotation.getDegrees() - getPose().getRotation().getDegrees()};
+    //     pathGoalPoseError.set(pathErrorArray);
+    // }
+
+    public void log(){
+        Arrays.stream(mModules).forEach(SwerveModule::log);
+        double[] swervePoseArray = {getPose().getX(), getPose().getY(), getPose().getRotation().getDegrees()};
+        swervePose.set(swervePoseArray);
+
+        // if (LimelightHelpers.getTV("")){
+        // Pose2d botPose = LimelightHelpers.getBotPose2d_wpiBlue("");
+        // double timeDelay = Timer.getFPGATimestamp() - (LimelightHelpers.getLatency_Capture("")/1000.0) - (LimelightHelpers.getLatency_Pipeline("")/1000.0);
+
+        // SmartDashboard.putNumber("limelightTIme", timeDelay);
+        // SmartDashboard.putNumber("limex", botPose.getY());
+        // SmartDashboard.putNumber("limey", botPose.getX());
+        // SmartDashboard.putNumber("limeRot", botPose.getRotation().getDegrees());
+        // SmartDashboard.putNumber("odometryRotation", mSwerveDrivePoseEstimator.getEstimatedPosition().getRotation().getDegrees());
+        // SmartDashboard.putNumber("Tags", LimelightHelpers.getLatestResults("").targetingResults.targets_Retro.length);
+        //}
+    }
+}
---
+++ src/main/java/org/deceivers/swerve/SwerveModuleV3.java
@@ -0,0 +1,182 @@
+package org.deceivers.swerve;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class SwerveModuleV3 implements SwerveModule {
+
+    private final CANSparkMax mAzimuthMotor;
+    private final CANSparkMax mDriveMotor;
+    private final AbsoluteEncoder mAzimuthAbsoluteEncoder;
+    private final RelativeEncoder mAzimuthIncrementalEncoder;
+    private final RelativeEncoder mDriveEncoder;
+    private final SparkMaxPIDController mDrivePID;
+    private final SparkMaxPIDController mAzimuthPID;
+    private final Translation2d mLocation;
+    private final String mName;
+
+    // need to update the speed to m/s
+
+    public SwerveModuleV3(CANSparkMax azimuthMotor, CANSparkMax driveMotor,
+            Translation2d location, String name) {
+
+        mDriveMotor = driveMotor;
+        mAzimuthMotor = azimuthMotor;
+        mLocation = location;
+        mName = name;
+
+        // Rest motors to factory defaults to ensure correct parameters
+        mDriveMotor.restoreFactoryDefaults();
+        mAzimuthMotor.restoreFactoryDefaults();
+
+        // Get encoders
+        mAzimuthAbsoluteEncoder = mAzimuthMotor.getAbsoluteEncoder(Type.kDutyCycle);
+        mAzimuthIncrementalEncoder = mAzimuthMotor.getEncoder();
+        mDriveEncoder = mDriveMotor.getEncoder();
+
+        // Get PIDs
+        mDrivePID = mDriveMotor.getPIDController();
+        mAzimuthPID = mAzimuthMotor.getPIDController();
+
+        // Configure drive motor controller parameters
+        mDriveMotor.setInverted(true);
+        mDriveMotor.setClosedLoopRampRate(0);
+        mDriveMotor.setOpenLoopRampRate(.1);
+        mDriveMotor.setIdleMode(com.revrobotics.CANSparkBase.IdleMode.kCoast);
+        mDriveMotor.setSmartCurrentLimit(40, 60, 5700);
+
+        // Configure Drive Encoder
+        mDriveEncoder.setPositionConversionFactor(0.2393893602 / ((18.0*45.0)/(12.0*15.0)));
+        mDriveEncoder.setVelocityConversionFactor(((0.2393893602 / ((18.0 / 12.0) * (45.0 / 15.0))) / 60));
+        mDriveEncoder.setPosition(0);
+
+        // Configure azimuth motor controller parameters
+        mAzimuthMotor.setInverted(true);
+        mAzimuthMotor.setClosedLoopRampRate(0);
+        mAzimuthMotor.setOpenLoopRampRate(0);
+        mAzimuthMotor.setIdleMode(com.revrobotics.CANSparkBase.IdleMode.kBrake);
+        mAzimuthMotor.setSmartCurrentLimit(20);
+
+        // Configure drive absolute encoder
+        mAzimuthAbsoluteEncoder.setPositionConversionFactor(360);
+        mAzimuthAbsoluteEncoder.setInverted(true);
+        mAzimuthAbsoluteEncoder.setAverageDepth(1);
+
+        // Configure azimuth incremental encoder
+        mAzimuthIncrementalEncoder.setPositionConversionFactor(360.0 / 35.94);
+
+        // Configure drive PID
+        mDrivePID.setFF(.3);
+        mDrivePID.setP(.1);
+
+        // Configure azimuth PID
+        mAzimuthPID.setFeedbackDevice(mAzimuthAbsoluteEncoder);
+        mAzimuthPID.setP(.05);
+        mAzimuthPID.setPositionPIDWrappingEnabled(true);
+        mAzimuthPID.setPositionPIDWrappingMinInput(0);
+        mAzimuthPID.setPositionPIDWrappingMaxInput(360);
+
+        // Burn flahs in case of power cycle
+        mDriveMotor.burnFlash();
+        mAzimuthMotor.burnFlash();
+    }
+
+    // Sets the drive motor speed in open loop mode
+    public void setSpeed(double speed) {
+        mDriveMotor.set(speed);
+    }
+
+    // Sets the rotation speed of the azimuth motor in open loop mode
+    public void setRotation(double rotation) {
+        mAzimuthMotor.set(rotation);
+    }
+
+    // Gets the speed of the drive motor
+    public double getSpeed() {
+        return mDriveEncoder.getVelocity();
+    }
+
+    // Gets the rotation position of the azimuth module
+    public double getRotation() {
+        return mAzimuthAbsoluteEncoder.getPosition();
+    }
+
+    // Gets the x/y location of the module relative to the center of the robot
+    @Override
+    public Translation2d getModuleLocation() {
+        return mLocation;
+    }
+
+    // Get the state (speed/rotation) of the swerve module
+    public SwerveModuleState getState() {
+        return new SwerveModuleState(getSpeed(), Rotation2d.fromDegrees(getRotation()));
+    }
+
+    // Run when swerve drive is first initialized
+    @Override
+    public void init() {
+
+    }
+
+    // Get the position of swerve modules (distance and angle)
+    public SwerveModulePosition getPosition() {
+        return new SwerveModulePosition(getDistance(), Rotation2d.fromDegrees(getRotation()));
+    }
+
+    // Get the distance of the drive encoder
+    public double getDistance() {
+        return mDriveEncoder.getPosition();
+    }
+
+    // Log swerve data
+    @Override
+    public void log() {
+        SmartDashboard.putNumber(mName + " Current", mDriveMotor.getOutputCurrent());
+        SmartDashboard.putNumber(mName + " Speed", mDriveEncoder.getVelocity());
+    }
+
+    // Set the speed and direction of the swerve module
+    @Override
+    public void set(SwerveModuleState drive) {
+        Rotation2d current = Rotation2d.fromDegrees(mAzimuthAbsoluteEncoder.getPosition());
+        SwerveModuleState optimizedState = SwerveModuleState.optimize(drive, current);
+        double setpoint = optimizedState.angle.getDegrees();
+        double velocity = optimizedState.speedMetersPerSecond;
+        mAzimuthPID.setReference(setpoint, com.revrobotics.CANSparkBase.ControlType.kPosition);
+        mDriveMotor.set(velocity);
+    }
+
+    @Override
+    public void setClosedLoop(SwerveModuleState drive) {
+        Rotation2d current = Rotation2d.fromDegrees(mAzimuthAbsoluteEncoder.getPosition());
+        SwerveModuleState optimizedState = SwerveModuleState.optimize(drive, current);
+        double setpoint = optimizedState.angle.getDegrees();
+        double velocity = optimizedState.speedMetersPerSecond;
+        mAzimuthPID.setReference(setpoint, com.revrobotics.CANSparkBase.ControlType.kPosition);
+        mDrivePID.setReference(velocity, com.revrobotics.CANSparkBase.ControlType.kVelocity);
+    }
+
+    // Stop all motors
+    @Override
+    public void stop() {
+        mAzimuthMotor.set(0);
+        mDriveMotor.set(0);
+    }
+
+    // set angle of swerve drive
+    @Override
+    public void setAngle(double angle) {
+        mAzimuthPID.setReference(angle, com.revrobotics.CANSparkBase.ControlType.kPosition);
+
+    }
+
+}
